bbc72fc63650ffbdc757ffb3c43aea7c940397fd
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-07-03 08:29:33 -0300

    To voltando a estudar angular

---
+++ src/app/meu-primeiro/meu.primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-componet',
+  template: `<p>Meu primeiro Component com Angular 2</p>`,
+})
+export class MeuPrimeiroComponent {}
