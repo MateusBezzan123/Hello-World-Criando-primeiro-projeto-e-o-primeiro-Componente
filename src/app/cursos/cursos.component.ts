519436dd7c9423d5971debd77b1c9a2a71cb3276
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-07-03 10:12:14 -0300

    Template

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext Js', 'Angular'];
 
-  constructor() { }
+  constructor() {
+    this.nomePortal = 'https://loiane.training/';
 
-  ngOnInit(): void {
+    for (let i = 0; i < this.cursos.length; i++) {
+      let curso = this.cursos[i];
+    }
   }
 
+  ngOnInit(): void {}
 }
